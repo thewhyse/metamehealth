c5b3429d900d5c47efeb537cdbe6a97980618a9f
Max <maxsevostianov@192.168.1.3>
2022-02-16 13:44:37 -0500

    mmh-office-address in the footer

--- wp-content/themes/metame/resources/views/partials/footer.blade.php
+++ wp-content/themes/metame/resources/views/partials/footer.blade.php
@@ -3,6 +3,19 @@
     <div class="row">
       <div class="col-12 col-lg-11">
         @php dynamic_sidebar('sidebar-footer') @endphp
+        <div class="row">
+          <div class="col-12 col-lg-11">
+            @php dynamic_sidebar('sidebar-footer') @endphp
+            <div id="mmh-office-address">
+              <span>metaMe Health Inc</span>
+              <span>222 Merchandise Mart Plz, Suite 1230 Chicago, IL 60654</span>
+              <span><a href="mailto: info@metameconnect.com" style="color: #54534a;">info@metameconnect.com</a></span>
+            </div>
+          </div>
+          <div class="col-12 col-lg-11">
+            <hr>
+          </div>
+        </div>
       </div>
       <div class="col-12 col-lg-11">
         <hr>
